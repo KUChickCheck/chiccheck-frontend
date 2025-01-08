5558d3697d7b6222e67d5cc781fe87d17434807b
breezjirasak <jirasak.tu@ku.th>
2025-01-09 02:08:27 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import FaceDetection from "./FaceDetection";
+import FaceLandmarkerComponent from "./FaceLandmarkerComponent";
+
+function App() {
+  return (
+    <div>
+      {/* <FaceDetection /> */}
+      <FaceLandmarkerComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FaceLandmarkerComponent.jsx
@@ -0,0 +1,156 @@
+import React, { useEffect, useRef, useState } from "react";
+// import axios from "axios";
+import {
+  FaceLandmarker,
+  FilesetResolver,
+} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0";
+import { drawMesh } from "./utilities";
+
+const FaceLandmarkDetection = () => {
+  const [faceLandmarker, setFaceLandmarker] = useState(null);
+  const [landmarks, setLandmarks] = useState([]);
+  const [liveness, setLiveness] = useState("Face not found");
+  const videoRef = useRef(null);
+  const liveViewRef = useRef(null);
+  const intervalRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  // Initialize the face landmarker
+  useEffect(() => {
+    const initializeFaceLandmarker = async () => {
+      const vision = await FilesetResolver.forVisionTasks(
+        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
+      );
+      const landmarker = await FaceLandmarker.createFromOptions(vision, {
+        baseOptions: {
+          modelAssetPath:
+            "https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task",
+          delegate: "GPU",
+        },
+        runningMode: "VIDEO",
+      });
+      setFaceLandmarker(landmarker);
+    };
+
+    initializeFaceLandmarker();
+
+    return () => {
+      if (intervalRef.current) clearInterval(intervalRef.current);
+    };
+  }, []);
+
+  // Enable webcam and start detection
+  const enableWebcam = async () => {
+    if (!faceLandmarker) {
+      alert("Face Landmarker is still loading. Please try again.");
+      return;
+    }
+
+    const constraints = { video: true };
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    videoRef.current.srcObject = stream;
+
+    videoRef.current.onloadeddata = () => {
+      startPrediction();
+      // Ensure canvas matches video size after it has loaded
+      const video = videoRef.current;
+      const canvas = canvasRef.current;
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+    };
+  };
+
+  // Start predictions at 30 FPS
+  const startPrediction = () => {
+    if (!faceLandmarker || !videoRef.current) return;
+
+    intervalRef.current = setInterval(() => {
+      const video = videoRef.current;
+      const currentTime = performance.now();
+
+      const result = faceLandmarker.detectForVideo(video, currentTime);
+      // setLandmarks(result.faceLandmarks[0]);
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      if (result.faceLandmarks.length !== 0) {
+
+        requestAnimationFrame(() => {
+          drawMesh(result.faceLandmarks, ctx, canvas);
+        });
+
+        const noseTip = result.faceLandmarks[0][1]
+        const leftCheek = result.faceLandmarks[0][234]
+        const rightCheek = result.faceLandmarks[0][454]
+        const Chin = result.faceLandmarks[0][152]
+        const forehead = result.faceLandmarks[0][10]
+
+        const depthNoseLeft = Math.abs(noseTip.z - leftCheek.z)
+        const depthNoseRight = Math.abs(noseTip.z - rightCheek.z)
+
+        // console.log("left: " + depthNoseLeft + ", z: " + leftCheek.z)
+        // console.log("right: " +depthNoseRight + ", z: " + rightCheek.z)
+
+        if (depthNoseLeft < 0.2 && depthNoseRight < 0.2) {
+          setLiveness("Spoof")
+        } else {
+          setLiveness("Live")
+        }
+      } else {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        setLiveness("Face not found")
+      }
+
+    }, 0); // Approximately 30 FPS
+  };
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>{liveness}</h1>
+      {/* Webcam Detection */}
+      <button onClick={enableWebcam} style={{ marginBottom: "10px" }}>
+        Enable Webcam
+      </button>
+      <div
+        id="liveView"
+        ref={liveViewRef}
+        style={{
+          position: "relative",
+          width: "100%",
+          height: "auto",
+          overflow: "hidden",
+        }}
+      >
+        <video
+          ref={videoRef}
+          autoPlay
+          playsInline
+          muted
+          style={{ width: "100%", height: "auto", position: "relative", zIndex: 1 }}
+        ></video>
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "auto",
+            zIndex: 2,
+          }}
+        ></canvas>
+      </div>
+    </div>
+  );
+};
+
+export default FaceLandmarkDetection;
+
