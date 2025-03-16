741e0e21975ed4d08f7956f1f69912f0a9291889
breezjirasak <jirasak.tu@ku.th>
2025-03-16 23:32:47 +0700

    test 3action

--- src/pages/main/FaceScan.jsx
+++ src/pages/main/FaceScan.jsx
@@ -61,7 +61,7 @@ const directionsList = ["Left", "Right", "Up", "Down", "Up-Left", "Up-Right", "D
 
 const getRandomDirections = () => {
   let shuffled = [...directionsList].sort(() => Math.random() - 0.5); // Shuffle the array
-  let randomDirections = shuffled.slice(0, 3); // Get 4 random directions
+  let randomDirections = shuffled.slice(0, 2); // Get 4 random directions
   // let randomDirections = ["Center"]
   randomDirections.push("Center");
   return randomDirections;
